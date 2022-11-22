34881e4f4f238ac708b0b24f7ec15593cdac3e1a
VolroDev <104170926+VolroDev@users.noreply.github.com>
2022-11-22 09:57:12 +0200

    22.11.2022

--- js/button.js
+++ js/button.js
@@ -1,15 +1,63 @@
-function sendForm(e){
-     
-    // получаем значение поля key
-    var keyBox = document.search.key;
-    var val = keyBox.value;
-    if(val.length>5){
-        alert("Недопустимая длина строки");
-        e.preventDefault();
-    }   
-    else
-        alert("Отправка разрешена");
+const numbers = "1234567890";
+const characters = "йцукенгшщзхїфівапролджєячсмитьбю";
+const specialCharacters = "~`!@#№$%^&*()-_=+[{]};:',<.>/?" + '"\|';
+let wordsCount;
+let numbersCount;
+let charactersCount;
+let specialCharactersCount;
+
+function sendForm() {
+
+    wordsCount = 0;
+    numbersCount = 0;
+    charactersCount = 0;
+    specialCharactersCount = 0;
+
+    let keyBox = document.getElementById("InputText").value;
+
+    if (keyBox.length > 0) {
+
+        let words = keyBox.split(' ');
+        wordsCount = words.length;
+
+        for (index = 0; index < words.length; ++index) {
+            wordAnalysis(words[index]);
+        }
+
+    }
+    else {
+
+    }
+
+    let resultText = "Кількість слів " + wordsCount + "\n";
+    resultText = resultText + "Кількість чисел " + numbersCount + "\n";
+    resultText = resultText + "Кількість звичайних символів " + charactersCount + "\n";
+    resultText = resultText + "Кількість спеціальних символів " + specialCharactersCount + "\n";
+
+    document.getElementById("OutputText").value = resultText;
+
+    //alert(1);
+
 }
- 
-var sendButton = document.search.send;
-sendButton.addEventListener("click", sendForm);
+
+function symbolAnalysis(element) {
+    if (characters.indexOf(element) > 0) {
+        charactersCount++;
+    }
+    else if (specialCharacters.indexOf(element) > 0) {
+        specialCharactersCount++;
+    }
+    else if (numbers.indexOf(element) > 0) {
+        specialCharactersCount++;
+    }
+    else {
+
+    }
+}
+
+function wordAnalysis(word) {
+    let array = word.split('');
+    for (let index = 0; index < array.length; index++) {
+        symbolAnalysis(array[index]);
+    }
+}
